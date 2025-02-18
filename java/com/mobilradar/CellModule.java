e28bce33e653602949c17083cf09065fed3271a4
papitvenge <116113304+papitvenge@users.noreply.github.com>
2025-02-18 09:55:26 +0100

    Create CellModule.java

---
+++ java/com/mobilradar/CellModule.java
@@ -0,0 +1,32 @@
+java
+package com.mobilradar;
+
+import android.telephony.CellInfo;
+import android.telephony.TelephonyManager;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Promise;
+
+public class CellModule extends ReactContextBaseJavaModule {
+    private TelephonyManager telephonyManager;
+
+    public CellModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        telephonyManager = (TelephonyManager) reactContext.getSystemService(reactContext.TELEPHONY_SERVICE);
+    }
+
+    @Override
+    public String getName() {
+        return "CellModule";
+    }
+
+    @ReactMethod
+    public void getCellInfo(Promise promise) {
+        try {
+            promise.resolve(telephonyManager.getAllCellInfo().toString());
+        } catch (Exception e) {
+            promise.reject("ERROR", e);
+        }
+    }
+}
